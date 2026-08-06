02786934a7bcf470136c12047d7aa034945d57f9
Akhil Benny <Akhil.b@applab.qa>
2026-08-06 15:44:58 +0530

    migration change

--- database/migrations/2026_08_06_090019_add_region_id_to_jobs_table.php
+++ database/migrations/2026_08_06_090019_add_region_id_to_jobs_table.php
@@ -9,18 +9,22 @@ return new class extends Migration
     public function up(): void
     {
         if (Schema::hasColumn('jobs', 'region_id')) {
+
+            DB::statement("
+                ALTER TABLE jobs
+                DROP FOREIGN KEY IF EXISTS jobs_region_id_foreign
+            ");
+
             Schema::table('jobs', function (Blueprint $table) {
-                try {
-                    $table->dropForeign(['region_id']);
-                } catch (\Exception $e) {
-                    // Ignore
-                }
                 $table->dropColumn('region_id');
             });
         }
 
         Schema::table('jobs', function (Blueprint $table) {
-            $table->foreignId('region_id')->nullable()->constrained('regions')->onDelete('set null');
+            $table->foreignId('region_id')
+                ->nullable()
+                ->constrained('regions')
+                ->nullOnDelete();
         });
     }
 

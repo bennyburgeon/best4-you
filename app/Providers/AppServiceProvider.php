5fbb5b6bb583cb2c9fd0e61dd9706a85468339b2
Akhil Benny <Akhil.b@applab.qa>
2026-08-06 17:08:09 +0530

    changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         URL::forceScheme('https');
+
+        view()->composer('*', function ($view) {
+            if (\Illuminate\Support\Facades\Schema::hasTable('regions')) {
+                $view->with('globalRegions', \App\Models\Region::where('status', true)->get());
+            }
+        });
     }
 }

cc13986043ce7103655b67b29fe14ded5f1fda66
Akhil Benny <Akhil.b@applab.qa>
2026-08-17 18:26:58 +0530

    dd

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.frontend')
-
+@dd('hh');
 @section('title', 'Best Job Consultancy in India | Best4U Careers')
 @section('meta_description', 'Find the best job consultancy in Kerala for freshers and experienced professionals. Explore the latest job opportunities and get reliable career guidance and placement support.')
 @section('canonical')
